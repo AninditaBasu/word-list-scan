ee6b4bc00f42e062458403821f6fb3ace1df6fe9
Anindita Basu <anindita.basu@live.com>
2015-06-12 00:12:17 +0530

    Update wordListScan.py

--- wordListScan.py
+++ wordListScan.py
@@ -1,54 +1,126 @@
 __author__ = 'anindita basu'
 import re
 import os
-
+#
+# Open a file called "wordListScan.html" to write the findings to.
+#
+reportfile = open("wordListScan.html", "w")
+reportfile.write("<html>")
+reportfile.write("<head>")
+reportfile.write("<title>Search report</title>")
+reportfile.write("<style>h1{color:#A52A2A;}")
+reportfile.write("h2{color:#A52A2A;}")
+reportfile.write("h3{color:#A52A2A;}")
+reportfile.write("h4{color:#A52A2A;}")
+reportfile.write('p{font-family:"Trebuchet MS", Arial, Verdana, serif; font-size:16px;line-height:120%;}')
+reportfile.write('li{font-family:"Trebuchet MS", Arial, Verdana, serif; font-size:16px;line-height:200%;}')
+reportfile.write('td{font-family:"Trebuchet MS", Arial, Verdana, serif; font-size:16px;line-height:200%;}</style>')
+reportfile.write("</head>")
+reportfile.write("<body>")
+reportfile.write("<h1>Search report</h1>")
+#
 fname = 'wordlist.txt'
+#
+# The name of the word list file is hardcoded. Users can change the contents of file.
+#
 wordlist = []
+#
+# Define a list to write the list of words in the wordlist.txt file to.
+#
+print ("\n")
+print 'Reading the word list ...\n'
+reportfile.write("<p>Words and phrases searched for: ")
+#
 fwordlist = open(fname)
 for word in fwordlist:
     word = word.rstrip()
     wordlist.append(word)
+    reportfile.write(word)
+    reportfile.write(", ")
     print word
 fwordlist.close()
-print '==============list of words read=============='
-
+reportfile.write("...end...</p>")
+#
+# Count the number of words in the list and print it out.
+#
 x = len(wordlist)
-print 'No. of words and phrases to search for: ', x
-
-workspace = raw_input("Enter the directory to be scanned: ")
-
+print '\nNo. of words and phrases to search for: ', x
+#
+# Get the directory that contains the DITA files.
+# Print it to the report file.
+#
+print "\nSpecify the full path of the directory to be scanned, for example, C:\jazz_repo\prodA\\\n"
+print "Do not forget the trailing slash\n"
+#
+workspace = raw_input("Enter the full path of directory to be scanned: ")
+#
+reportfile.write('<p>Directory scanned: <a href = "file:///')
+reportfile.write(workspace)
+reportfile.write('" target = "_blank">')
+reportfile.write(workspace)
+reportfile.write("</a></p>")
+reportfile.write("<hr/>")
+#
+# start a counter for the number of subfolders in the directory
+#
 counter = 0
+#
+# Print out the subfolders. Also, count them and print out the count.
+#
+print '\nReading the directory ...\n'
+#
 try:
     for foldername in os.listdir(workspace):
         counter += 1
         print foldername
 except:
-    print 'The directory could not be found'
+    print 'The directory could not be found.\n'
     exit()
-print 'The directory has ', counter, 'folders'
-
+print '\nThe program will scan ', counter, 'folders in the', workspace, 'directory.\n'
+#
+# Start a counter for the number of dita files in the directory
+#
+# If file is a dita file, create the full file path. Then, pick up the wordlist.
+# For each word in the list, open the file, search for occurrence, add to wordcounter if found,
+# print the filename only if searched word exists in the file, and then close the file.
+#
 counter = 0
 for (dirname, dirs, files) in os.walk(workspace):
     for filename in files:
         if filename.endswith('.dita'):
             counter = counter + 1
-            #print filename
+            # print filename
             thefile = os.path.join(dirname, filename)
-            wcount = 0
             for word in wordlist:
                 handle = open(thefile)
                 for line in handle:
                     line = line.rstrip()
-                    if re.search(word, line):
-                        wcount += 1
-            if wcount >= 1:
-                print "In", thefile, word, 'occurs', wcount, 'times'
-            wcount = 0
-            handle.close()
-print 'The directory has', counter, 'DITA files'
-
-raw_input('==============finished printing word occurrences==============')
-
-raw_input("press any key to close")
+                    if re.findall(word, line):
+                        print word, 'occurs', "in", thefile
+                        reportfile.write("<p>")
+                        reportfile.write(' <span style="color: red">')
+                        reportfile.write(word)
+                        reportfile.write("</span> occurs in ")
+                        reportfile.write(thefile)
+                        reportfile.write(".</p>")
+                handle.close()
+print '\nFinished checking', counter, 'DITA files.\n'
+#
+# print a completion message
+#
+print '==============finished printing word occurrences==============\n'
+#
+#
+# Write the closing HTML tags in the report file.
+#
+reportfile.write("<hr/>")
+reportfile.write("</body>")
+reportfile.write("</html>")
+reportfile.close()
+#
+# Wait for user input so that a person gets enough time to read whatever's been printed on the console
+# and then close the program
+#
+print "The program will now end."
+raw_input("Press any key to close.")
 exit()
-Enter file contents here
